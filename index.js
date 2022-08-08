bf6d3e7982d2207d6df1d23b9f74dcb78cbd277a
Alexander R <ru5nedug@mail.ru>
2022-08-08 14:24:39 +0300

    bcryptjs

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import express from 'express';
 import config from 'config';
 
 
-const app = express();
+const app = express(); /* Создаем приложение */
 
-const PORT = config.get('serverPort');
+const PORT = config.get('serverPort'); /* Получааем порт */
 
 
 const start = async () => {
     try {
-        await new mongoose.connect(config.get('dbURL'),
+        await new mongoose.connect(config.get('dbURL'), /* Обращаемся к MongoDB */
             // {
             //     useNewUrlParser: true,
             //     useUnifiedTopology: true,
@@ -18,13 +18,11 @@ const start = async () => {
             // }
         );
 
-
         app.listen(PORT, () => {
-            console.log('Server started on port: ', PORT);
+            console.log('Server started on port: ', PORT); /* Слушаем сервер на нужном порту */
         });
-
     } catch (e) {
-
+        console.log(e.message);
     }
 };
 
