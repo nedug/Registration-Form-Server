3730728b94333ec702c64eaf858cc58e2a733b9b
Alexander R <ru5nedug@mail.ru>
2022-08-08 17:27:22 +0300

    corsMiddleware

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import mongoose from 'mongoose';
 import express from 'express';
 import config from 'config';
 import { authRouter } from './routes/auth.routes.js';
+import { corsMiddleware } from './middleware/cors.middleware.js';
 
 
 const app = express(); /* Создаем приложение */
 
 const PORT = config.get('serverPort'); /* Получааем порт */
 
-
+app.use(corsMiddleware); /* CORS */
 app.use(express.json()); /* Работаем с JSON */
 app.use('/api/auth', authRouter); /* Обрабатываем роуты /api/auth */
 
